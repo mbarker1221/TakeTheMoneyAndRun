81eea6b7ded31e45d509bfde844cc9720c2a2778
michelle barker <mbarker1221@gmail.com>
2018-01-02 23:10:07 -0500

    added new feed to news page and corrected syntax errors causing long page load

--- index.js
+++ index.js
@@ -212,24 +212,29 @@ function showNews(results) {
    let article = `
   
    
-   <a class="link" href=""${results.articles[0].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[0].urlToImage}"></a><br />
+   <a class="link" href="${results.articles[0].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[0].urlToImage}"></a><br />
    
-   <a class="link" href="${results.articles[0].url}" target="_blank">${results.articles[0].title}</a><br /><br /><br />
+   <a class="title" href="${results.articles[0].url}" target="_blank">${results.articles[0].title}<alt="${results.articles[0].description}"></a><br /><br /><br />
    
    
-    <a class="link" href="${results.articles[1].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[1].urlToImage}"></a><br />
+   <a class="link" href="${results.articles[1].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[1].urlToImage}"></a><br />
    
-   <a class="title" href="${results.articles[1].url}" target="_blank">${results.articles[1].title}</a><br /><br /><br />
+   <a class="title" href="${results.articles[1].url}" target="_blank">${results.articles[1].title} <alt="${results.articles[1].description}"></a><br /><br /><br />
    
   
-    <a class="link" href="${results.articles[2].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[2].urlToImage}"</a><br />
+   <a class="link" href="${results.articles[2].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[2].urlToImage}"></a><br />
    
-   <a class="title" href="${results.articles[2].url}" target="_blank">${results.articles[2].title}></a><br /><br /><br />
+   <a class="title" href="${results.articles[2].url}" target="_blank">${results.articles[2].title}<alt="${results.articles[2].description}"></a><br /><br /><br />
    
    
-    <a class="link href="${results.articles[3].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[3].urlToImage}"></a><br />
+   <a class="link" href="${results.articles[3].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[3].urlToImage}"></a><br />
    
-   <a class="title" href="${results.articles[3].url}" target="_blank">${results.articles[3].title}</a><br /><br /><br />
+   <a class="title" href="${results.articles[3].url}" target="_blank">${results.articles[3].title}<alt="${results.articles[3].description}"></a><br /><br />
+   
+    <a class="link" href="${results.articles[4].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[4].urlToImage}"></a><br />
+   
+   <a class="title" href="${results.articles[4].url}" target="_blank">${results.articles[4].title} <alt="${results.articles[4].description}"></a><br /><br /><br />
+  
    
    `;
    $('.stories').html(article);
